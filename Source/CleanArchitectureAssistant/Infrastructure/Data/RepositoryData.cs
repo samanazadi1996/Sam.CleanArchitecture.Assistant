c1ba6ee7937f484400b2c8acad1e4c27a983e7ae
saman azadi <samanazadi1996@gmail.com>
2024-12-01 00:19:07 +0330

    2 files modified and 2 files added
    Modified : Source/CleanArchitectureAssistant/CleanArchitectureAssistant.csproj
    Modified : Source/CleanArchitectureAssistant/Infrastructure/Data/EntityData.cs
    Added : Source/CleanArchitectureAssistant/Infrastructure/Data/RepositoryData.cs
    Added : Source/CleanArchitectureAssistant/Infrastructure/Services/RepositoryService.cs

---
+++ Source/CleanArchitectureAssistant/Infrastructure/Data/RepositoryData.cs
@@ -0,0 +1,42 @@
+﻿using CleanArchitectureAssistant.Infrastructure.DTOs;
+
+namespace CleanArchitectureAssistant.Infrastructure.Data;
+
+public class RepositoryData
+{
+    public static FileDto GetInterface(string solutionName, string entityName)
+    {
+        var result = new FileDto($"I{entityName}Repository.cs")
+        {
+            Path = "ca-repo\\IProductRepository.ca"
+        };
+
+        var fileContent = EmbeddedResourceDataReader.ReadEmbeddedTextFile(result.Path);
+
+
+        result.Content = fileContent
+            .Replace("Product", entityName)
+            .Replace("CleanArchitecture.", solutionName + ".");
+
+        return result;
+
+    }
+    public static FileDto GetImplementation(string solutionName, string entityName)
+    {
+        var result = new FileDto($"{entityName}Repository.cs")
+        {
+            Path = "ca-repo\\ProductRepository.ca"
+        };
+
+        var fileContent = EmbeddedResourceDataReader.ReadEmbeddedTextFile(result.Path);
+
+
+        result.Content = fileContent
+            .Replace("Product", entityName)
+            .Replace("CleanArchitecture.", solutionName + ".");
+
+        return result;
+
+    }
+
+}
---
+++ Source/CleanArchitectureAssistant/Infrastructure/Services/RepositoryService.cs
@@ -0,0 +1,32 @@
+﻿using CleanArchitectureAssistant.Infrastructure.Data;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace CleanArchitectureAssistant.Infrastructure.Services;
+
+public class RepositoryService
+{
+    public static async Task<bool> AddRepository(string entityName)
+    {
+        try
+        {
+            var domainPath = await CommonService.GetDomainPath();
+            if (string.IsNullOrEmpty(domainPath))
+                return false;
+
+            var @interface = RepositoryData.GetInterface(await CommonService.GetSolutionName(), entityName);
+
+            var @implementation = RepositoryData.GetImplementation(await CommonService.GetSolutionName(), entityName);
+
+            //File.WriteAllText(Path.Combine(dir, file.Name), file.Content);
+
+        }
+        catch
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+}
